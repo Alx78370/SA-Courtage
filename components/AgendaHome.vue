2912cd3e13efc62ef2e30b0172350dca3ece0e1c
Alexis VACHET <alexis.vachet@outlook.fr>
2024-11-27 23:52:08 +0100

    calendly implementé

--- components/AgendaHome.vue
+++ components/AgendaHome.vue
@@ -1,10 +1,21 @@
+<script setup lang="ts">
+const options = {
+  url: 'https://calendly.com/s-a-courtage/30min',
+}
+</script>
+
 <template>
-  <div>
-    <h3 class="text-4xl pb-3">
-      Discutons de votre projet ensemble
-    </h3>
-    <p>
-      À l'écoute de mes clients, transparente et soucieuse de leur offrir le meilleur service, il me fera plaisir de vous accompagner du début à la fin dans la réalisation de votre projet. Au plaisir de vous rencontrer !
-    </p>
+  <div class="shadow-2xl">
+    <div class="p-6">
+      <h3 class="text-4xl pb-3">
+        Discutons de votre projet ensemble
+      </h3>
+      <p>
+        À l'écoute de mes clients, transparente et soucieuse de leur offrir le meilleur service, il me fera plaisir de vous accompagner du début à la fin dans la réalisation de votre projet. Au plaisir de vous rencontrer !
+      </p>
+    </div>
+    <div>
+      <CalendlyInlineWidget class="rounded-e-xl" v-bind="options" />
+    </div>
   </div>
 </template>
