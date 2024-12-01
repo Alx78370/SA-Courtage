fa5cad5909c7b208399cb965c7e74d1faab3a17b
Alexis VACHET <alexis.vachet@outlook.fr>
2024-12-01 22:48:58 +0100

    footer

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-slate-100 text-black text-sm text-center p-5">
+    <p>&copy; 2024 S.A-Courtage</p>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,4 +5,7 @@
   <main class="bg-slate-100">
     <slot />
   </main>
+  <footer>
+    <Footer />
+  </footer>
 </template>
