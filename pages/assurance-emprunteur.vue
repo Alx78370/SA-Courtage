7cfe8788e9e8a5d3e56e97f9b94cc793df425c38
Alexis VACHET <alexis.vachet@outlook.fr>
2024-12-06 02:12:40 +0100

    mise en forme etxt assurance emprunteur

--- pages/assurance-emprunteur.vue
+++ pages/assurance-emprunteur.vue
@@ -14,16 +14,21 @@ import coupleContent from '@/assets/images/couple_content.webp'
     <div class="text-lg border-2 shadow-2xl rounded-3xl p-5 m-5">
       <p class="pb-5">
         Nouvelle assurance ou changement d'assurance de prêt ? Je compare les offres et vous garantis une couverture adaptée à votre situation, au meilleur tarif.
-
+      </p>
+      <p class="pb-5">
         Devis gratuit :
         Pour vérifier l'économie réalisable avec un changement d'assurance sur votre prêt immobilier en cours.
-
-        Procédure :
-
-        Téléchargez le document (Word ou PDF).
-        Transmettez-moi les documents demandés par mail pour une étude gratuite.
       </p>
-      <div class="flex justify-center">
+      <div>
+        <p class="pb-5">
+          Procédure :
+        </p>
+        <ul class="list-disc list-inside">
+          <li>Téléchargez le document.</li>
+          <li>Transmettez-moi les documents demandés via le formulaire ci-dessous pour une étude gratuite.</li>
+        </ul>
+      </div>
+      <div class="flex justify-center pt-5">
         <a href="@/public/PPS.pdf " download="PPS.pdf" class="bg-blue-500 rounded-3xl p-3 text-white">Télécharger le document</a>
       </div>
     </div>
