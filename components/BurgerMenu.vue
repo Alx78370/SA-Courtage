b8c69fc09a24a94671d6e7a97d9bb9e76d6ae45c
Alexis VACHET <alexis.vachet@outlook.fr>
2024-12-12 16:44:15 +0100

    hero responsive

---
+++ components/BurgerMenu.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps({
+  isMenuOpen: Boolean,
+});
+
+const _emit = defineEmits(['toggleMenu']);
+</script>
+
+<template>
+  <div class="md:hidden">
+    <button
+      class="text-gray-800 focus:outline-none"
+      @click="$emit('toggleMenu')"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16m-7 6h7"
+        />
+      </svg>
+    </button>
+  </div>
+</template>
--- components/HeroCard.vue
+++ components/HeroCard.vue
@@ -1,29 +1,32 @@
 <template>
   <div
-    class="flex flex-col text-white pb-5 px-5"
+    class="flex flex-col text-white pb-5 px-5 md:items-center"
   >
-    <h1 class="flex flex-col text-4xl font-medium pb-3">
+    <h1 class="flex flex-col text-4xl font-medium pb-3 md:items-center lg:text-5xl">
       Sylvie Allard
-      <span class="text-lg font-normal">Courtier en prêt immobilier et assurance</span>
+      <span class="text-lg font-normal lg:text-2xl">Courtier en prêt immobilier et assurance</span>
     </h1>
     <hr class="pb-3">
-    <div class="flex items-center gap-3">
-      <Icon name="system-uicons:location" />
-      78370, Plaisir
+    <div class="flex flex-col lg:text-lg">
+      <div class="flex items-center gap-3">
+        <Icon name="system-uicons:location" />
+        78370, Plaisir
+      </div>
+      <div class="flex items-center gap-3">
+        <Icon name="bi:phone" />
+        06 40 96 02 03
+      </div>
+      <div class="flex items-center gap-3 pb-6">
+        <Icon name="lets-icons:e-mail" />
+        s.a.courtage@gmail.com
+      </div>
     </div>
-    <div class="flex items-center gap-3">
-      <Icon name="bi:phone" />
-      06 40 96 02 03
-    </div>
-    <div class="flex items-center gap-3 pb-6">
-      <Icon name="lets-icons:e-mail" />
-      s.a.courtage@gmail.com
-    </div>
+
     <div class="flex justify-center gap-5">
-      <NuxtLink to="/contact" class="flex justify-center items-center bg-blue-600 p-3 rounded-2xl min-w-32">
+      <NuxtLink to="/contact" class="flex justify-center items-center bg-blue-600 font-semibold p-3 rounded-2xl min-w-32 lg:text-lg transition delay-75 duration-300 ease-in-out hover:scale-110">
         Contactez-moi
       </NuxtLink>
-      <NuxtLink to="/a-propos" class="flex justify-center items-center bg-slate-100 text-black p-3 rounded-2xl min-w-32">
+      <NuxtLink to="/a-propos" class="flex justify-center items-center bg-slate-100 text-black font-semibold p-3 rounded-2xl min-w-32 lg:text-lg transition delay-75 duration-300 ease-in-out hover:scale-110">
         En savoir plus
       </NuxtLink>
     </div>
