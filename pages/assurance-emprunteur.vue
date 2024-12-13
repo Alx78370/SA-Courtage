0e35b6aa526548e49c5a5cb909c84a940b451b41
Alexis VACHET <alexis.vachet@outlook.fr>
2024-12-14 00:42:45 +0100

    ajout contenu assurance emprunteur

--- pages/assurance-emprunteur.vue
+++ pages/assurance-emprunteur.vue
@@ -15,6 +15,13 @@ import coupleContentBanner from '@/assets/images/couple_content_banner.webp'
       <h2 class="text-2xl text-center font-semibold pb-10 md:text-3xl">
         Nouvelle assurance ou changement d'assurance de prêt ?
       </h2>
+      <p class="pb-2 font-medium">Que garantit l’assurance emprunteur ?</p>
+      <p class="pb-5">
+        L’assurance emprunteur est une assurance qui garantit la prise en charge de tout ou partie des échéances de remboursement ou du capital restant dû d’un crédit en cas de survenance de certains événements.
+      </p>
+      <p class="pb-5">
+        Ces événements sont le plus souvent le décès, la perte totale et irréversible d’autonomie, l’invalidité permanente, l’incapacité temporaire de travail (ITT).
+      </p>
       <p class="pb-5">
         Je compare les offres et vous garantis une couverture adaptée à votre situation, au meilleur tarif.
       </p>
