4a812cf596cd52a6a8bbe830d726a08262156141
Alexis VACHET <alexis.vachet@outlook.fr>
2025-02-02 19:12:01 +0100

    fix mobile heroCard

--- components/HeroCard.vue
+++ components/HeroCard.vue
@@ -10,14 +10,14 @@
     >
       <div class="flex flex-col items-center text-gray-800 bg-[#f1f5f9bd] rounded-2xl p-5 md:p-10">
         <h1
-          class="flex flex-col text-4xl font-semibold md:items-center lg:text-6xl"
+          class="flex flex-col text-2xl font-semibold md:items-center lg:text-6xl"
         >
           Sylvie Allard
         </h1>
-        <h2 class="text-lg text-center lg:text-2xl pt-1 font-semibold pb-3 border-b-2 border-gray-800">
+        <h2 class="text-lg text-center lg:text-xl pt-1 font-semibold pb-3 border-b-2 border-gray-800">
           Courtier en prêt immobilier et assurance
         </h2>
-        <div class="flex flex-col items-start lg:text-2xl pt-3">
+        <div class="flex flex-col items-start md:text-2xl pt-3">
           <div class="flex flex-col mx-auto">
             <div class="flex items-center gap-3">
               <Icon name="system-uicons:location" />
@@ -35,13 +35,13 @@
           <div class="flex justify-center gap-5">
             <NuxtLink
               to="/contact"
-              class="flex justify-center items-center bg-blue-600 font-semibold p-3 md:p-5 rounded-2xl  min-w-32 text-xl transition-all delay-75 duration-300 ease-in-out hover:scale-110 hover:bg-blue-700 text-white"
+              class="flex justify-center items-center bg-blue-600 font-semibold p-3 md:p-5 rounded-2xl  min-w-32 md:text-xl transition-all delay-75 duration-300 ease-in-out hover:scale-110 hover:bg-blue-700 text-white"
             >
               Contactez-moi
             </NuxtLink>
             <NuxtLink
               to="/a-propos"
-              class="flex justify-center items-center bg-slate-100 text-gray-800 border-2 font-semibold p-3 md:p-5 rounded-2xl min-w-32 text-xl transition-all delay-75 duration-300 ease-in-out hover:scale-110 hover:bg-slate-200"
+              class="flex justify-center items-center bg-slate-100 text-gray-800 border-2 font-semibold p-3 md:p-5 rounded-2xl min-w-32 md:text-xl transition-all delay-75 duration-300 ease-in-out hover:scale-110 hover:bg-slate-200"
             >
               En savoir plus
             </NuxtLink>
