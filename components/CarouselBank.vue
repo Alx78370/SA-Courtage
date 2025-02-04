dc2811162b6e00a020c914a111b8f58807698bff
Alexis VACHET <alexis.vachet@outlook.fr>
2025-02-04 14:48:18 +0100

    added banks to carousel

--- components/CarouselBank.vue
+++ components/CarouselBank.vue
@@ -11,14 +11,34 @@ const images = ref([
     alt: "Logo Crédit Agricole",
   },
   {
+    itemImageSrc: "/logo/la-banque-postale-logo.svg",
+    thumbnailImageSrc: "/logo/la-banque-postale-logo.svg",
+    alt: "Logo La Banque Postale",
+  },
+  {
     itemImageSrc: "/logo/bnp-paribas-logo.svg",
     thumbnailImageSrc: "/logo/bnp-paribas-logo.svg",
     alt: "Logo BNP Paribas",
   },
   {
+    itemImageSrc: "/logo/CIC-logo.svg",
+    thumbnailImageSrc: "/logo/CIC-logo.svg",
+    alt: "Logo CIC",
+  },
+  {
     itemImageSrc: "/logo/caisse-d-epargne-logo.svg",
     thumbnailImageSrc: "/logo/caisse-d-epargne-logo.svg",
     alt: "Logo Caisse d'Épargne",
+  },
+  {
+    itemImageSrc: "/logo/CCF-logo.svg",
+    thumbnailImageSrc: "/logo/CCF-logo.svg",
+    alt: "Logo CCF",
+  },
+  {
+    itemImageSrc: "/logo/bred-logo.svg",
+    thumbnailImageSrc: "/logo/bred-logo.svg",
+    alt: "Logo Bred",
   },
   {
     itemImageSrc: "/logo/credit-mutuel-logo.svg",
@@ -31,14 +51,9 @@ const images = ref([
     alt: "Logo Banque Populaire",
   },
   {
-    itemImageSrc: "/logo/credit-mutuel-logo.svg",
-    thumbnailImageSrc: "/logo/credit-mutuel-logo.svg",
+    itemImageSrc: "/logo/LCL-logo.svg",
+    thumbnailImageSrc: "/logo/LCL-logo.svg",
     alt: "Logo LCL",
-  },
-  {
-    itemImageSrc: "/logo/logo-la-banque-postale-1.svg",
-    thumbnailImageSrc: "/logo/logo-la-banque-postale-1.svg",
-    alt: "Logo La Banque Postale",
   },
 ]);
 
@@ -62,7 +77,7 @@ const responsiveOptions = ref([
 </script>
 
 <template>
-  <div class="flex flex-col gap-20 ">
+  <div class="flex flex-col gap-20 px-5 md:px-10 xl:px-20">
     <h2 class="text-center pb-5 text-2xl md:text-3xl font-semibold px-5">Un réseau bancaire national avec un accès privilégié</h2>
     <Carousel
       :value="images"
@@ -71,7 +86,7 @@ const responsiveOptions = ref([
       :responsive-options="responsiveOptions"
       :autoplay-interval="2000"
       :show-item-navigators="false"
-      :show-navigators="false"
+      :show-navigators="true"
     >
       <template #item="slotProps">
         <img
