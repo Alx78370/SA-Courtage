5fbc79fdf50f16dca5b612067833553504d232ee
Alexis VACHET <alexis.vachet@outlook.fr>
2025-02-04 15:24:43 +0100

    modif footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="bg-slate-100 text-black text-sm text-center p-5">
-    <p>&copy; 2024 S.A-Courtage - créé par Alexis Vachet</p>
+    <p>&copy; 2025 S.A-Courtage - Créé par Alexis Vachet</p>
   </div>
 </template>
